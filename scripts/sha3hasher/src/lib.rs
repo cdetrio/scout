b1260d1b6a827cdc498441a5f7c4f3468d9614d2
cdetrio <cdetrio@gmail.com>
2019-07-15 00:12:09 -0400

    do sha3hasher with 100 iterations, and include assemblyscript wasm equivalent

--- scripts/sha3hasher/src/lib.rs
+++ scripts/sha3hasher/src/lib.rs
@@ -14,20 +14,54 @@ pub extern "C" fn main() {
 
     //let mut hash = [0u8; 32];
     // initialize 200 32-byte buffers
-    let mut hash_outputs = [[0u8; 32]; 200];
+    let mut hash_outputs = [[0u8; 32]; 100];
+
+
+    /*
+    // would like to test a for loop instead of an iter_mut()
+    // but rust won't let me iteratate over a 2d slice using a for loop
+    for i in 0u8..10 { // rust will not let me take hash_outputs[i] lol
+        block_data[0] = i;
+
+        tiny_keccak::Keccak::keccak256(&block_data[..], &mut hash_outputs[i]); // rust won't let us
+    }
+    */
+
+
+    /*
+    let mut hash_output = [0u8; 32];
+    tiny_keccak::Keccak::keccak256(&block_data[..], &mut hash_output);
+    let post_state_root = ewasm_api::types::Bytes32::from(hash_output);
+    */
+
 
     let mut i = 0;
-    //for i in 0u8..200 { // rust will not let me take hash_outputs[i] lol
     for output in hash_outputs.iter_mut() {
         block_data[0] = i; // mutate first byte of the block data just to get a different hash for the result
         i = i + 1;
 
-        //tiny_keccak::Keccak::keccak256(&block_data[..], &mut hash_outputs[i]);
-        tiny_keccak::Keccak::keccak256(&block_data[..], output);
+        tiny_keccak::Keccak::keccak256(&block_data[..], output); // this works
     }
 
-    // No updates were made to the state
-    let post_state_root = pre_state_root;
+
+
+    /*
+    let mut keccak256 = tiny_keccak::Keccak::new_keccak256();
+    let mut i = 0;
+    for output in hash_outputs.iter_mut() {
+        block_data[0] = i; // mutate first byte of the block data just to get a different hash for the result
+        i = i + 1;
+
+        keccak256.update(&block_data[..]);
+        keccak256.finalize(output);
+        keccak256.reset();
+    }
+    */
+
+
+
+    // use result of the second iteration as a test vector
+    let post_state_root = ewasm_api::types::Bytes32::from(hash_outputs[1]);
 
     eth2::save_post_state_root(post_state_root)
 }
